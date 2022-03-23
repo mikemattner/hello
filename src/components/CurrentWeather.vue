6b8bbd17c6e47279d90092210f07e827a6fbc16d
mikemattner <info@mikemattner.com>
2022-03-23 15:15:33 -0400

    Adding a loading animation.

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -1,33 +1,38 @@
 <template>
   <div class="current-weather">
-    <WeatherIcon
-      v-if="isInitialized"
-      :icon="weatherIcon"
-      :description="getDescription"
-    ></WeatherIcon>
-    <CurrentTemperature
-      v-if="isInitialized"
-      :temperature-value="getTemperatureValue"
-      :high="getTemperatureHigh"
-      :low="getTemperatureLow"
-      :location="getLocation"
-    ></CurrentTemperature>
+    <template v-if="!isInitialized">
+      <CurrentWeatherSkeleton />
+    </template>
+    <template v-if="isInitialized">
+      <WeatherIcon
+        :icon="weatherIcon"
+        :description="getDescription"
+      ></WeatherIcon>
+      <CurrentTemperature
+        :temperature-value="getTemperatureValue"
+        :high="getTemperatureHigh"
+        :low="getTemperatureLow"
+        :location="getLocation"
+      ></CurrentTemperature>
+    </template>
   </div>
 </template>
 
 <script lang="ts">
-import { onBeforeMount, defineComponent, reactive } from 'vue'
+import { onBeforeMount, defineComponent } from 'vue'
 import CurrentTemperature from '@/components/CurrentTemperature.vue';
 import WeatherIcon from '@/components/WeatherIcon.vue';
 import { useWeatherStore } from '@/stores/weather';
 import { storeToRefs } from 'pinia';
+import CurrentWeatherSkeleton from './CurrentWeatherSkeleton.vue';
 
 export default defineComponent({
   name: 'CurrentWeather',
   components: {
     CurrentTemperature,
     WeatherIcon,
-  },
+    CurrentWeatherSkeleton,
+},
   setup() {
     const weatherStore = useWeatherStore();
     const appId = import.meta.env.VITE_APP_ID;
@@ -35,7 +40,6 @@ export default defineComponent({
     onBeforeMount(async () => {
       if (navigator.onLine) {
         navigator.geolocation.getCurrentPosition(async (position) =>
-          //  currentPosition = position.coords
           await weatherStore.hydrateStore(appId, position.coords)
         );
       }
---
+++ src/components/CurrentWeatherSkeleton.vue
@@ -0,0 +1,179 @@
+<template>
+    <div class="temperature-skeleton">
+        <section class="weather-icon">
+            <div class="icon-container">
+            </div>
+        </section>
+        <section class="weather-data">
+            <div class="temperature-value-block">
+                <div class="weather-data__left">
+                    <div class="temp-block current-temperature__value">
+                    </div>
+                </div>
+                <div class="weather-data__right">
+                    <div class="temp-block temperature__high">
+                    </div>
+                    <div class="temp-block temperature__low">
+                    </div>
+                </div>
+            </div>
+            <div class="weather-data__location">
+                <div class="temp-block">
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped lang="scss">
+.temperature-skeleton {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.weather-icon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.icon-container {
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+    background-color: rgba(255,255,255,0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 20px;
+    overflow: hidden;
+    position: relative;
+
+    &:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        cursor: progress;
+        background: linear-gradient(0.25turn, transparent, rgba(255,255,255,0.125), transparent);
+        background-repeat: no-repeat;
+        background-position: -100px 0;
+        animation: loading 1.5s infinite;
+        z-index: 2;
+    }
+}
+
+.temp-block {
+    background-color: rgba(255,255,255,0.1);
+    aspect-ratio: 1;
+    overflow: hidden;
+    position: relative;
+
+    &:after {
+        content: '';
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        cursor: progress;
+        background: linear-gradient(0.25turn, transparent, rgba(255,255,255,0.125), transparent);
+        background-repeat: no-repeat;
+        background-position: -100px 0;
+        animation: loading 1.5s infinite;
+        z-index: 2;
+    }
+}
+
+@keyframes loading {
+  to {
+    background-position: 100px 0;
+  }
+}
+
+.weather-data {
+    height: 60px;
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-direction: column;
+    line-height: 1;
+    &__location {
+        font-size: 0.675rem;
+        margin-top: 0;
+        width: 100%;
+        height: 12px;
+        margin-top: 10px;
+        .temp-block {
+            width: 100%;
+            height: 12px;
+        }
+    }
+    .temperature-value-block {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    &__left,
+    &__right {
+        display: flex;
+    }
+    &__left {
+        justify-content: center;
+        align-items: flex-start;
+        margin-right: 0.5rem;
+        width: 80px;
+        height: 60px;
+
+        .temp-block {
+            width: 80px;
+            height: 60px;
+        }
+    }
+    &__right {
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+    }
+    &__value {
+        font-size: 2rem;
+        font-weight: 700;
+    }
+    .temperature__scale {
+        margin-top:0.125rem;
+        font-size: 1rem;
+        font-weight: 100;
+        a {
+        color: #fff;
+        text-decoration: none;
+        transition: all 0.25s ease-in-out;
+        &:hover {
+            color: rgba(255,255,255,0.5);
+        }
+        }
+    }
+    .temperature__high,
+    .temperature__low {
+        margin: 0.125rem 0;
+        font-size: 0.675rem;
+        display: flex;
+        justify-content: center;
+        width: 12px;
+        height: 12px;
+    }
+}
+</style>
