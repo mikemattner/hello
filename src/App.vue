7d877c858ab9873b7c3b67988025067ccc4b982f
mikemattner <info@mikemattner.com>
2022-04-10 19:34:04 -0400

    Moving about info to an about view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="main-view">
-    <RouterView />
+    <AppHeader />
+    <RouterView v-slot="{ Component }">
+      <Transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
+import { defineComponent, onBeforeMount } from 'vue';
+import { useWeatherStore } from '@/stores/weather';
+import AppHeader from '@/components/AppHeader.vue';
 
 export default defineComponent({
+  components: {
+    AppHeader,
+  },
   name: 'DashboardApp',
+  setup() {
+    const weatherStore = useWeatherStore();
+    onBeforeMount(async () => {
+      if (navigator.onLine) {
+        navigator.geolocation.getCurrentPosition(async (position) => await weatherStore.hydrateStore(position.coords));
+      }
+    });
+  },
 });
 </script>
 
@@ -22,7 +40,16 @@ export default defineComponent({
 .main-view {
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: 1fr;
+  grid-template-rows: auto 1fr;
   padding: 0 20px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 100ms ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
